64277b3a83c4be5e52f5e283e5d039bed95edca6
tinytweaks <relic7478@gmail.com>
2022-12-26 23:51:20 +0600

    Cute Commit

--- src/component.js
+++ src/component.js
@@ -1,10 +1,9 @@
 import { useState } from 'react'
-import { Link } from 'react-router-dom'
 import banner from './photo/banner.png'
 
 export function Navbar() {
 
-    const emojies = [':)', ':D', ';)', '(•‿•)', 'B)', ':>', ';D', ':o' , '(⌐■_■)', '(◕⋏◕)', 'ʕ•́ᴥ•̀ʔ', 'ツ', '◉_◉']
+    const emojies = [':)  ', ':D  ', ';)  ', '(•‿•)', 'B)  ', ':>  ', ';D  ', ':o  ' , '(⌐■_■)', '(◕⋏◕)', 'ʕ•́ᴥ•̀ʔ', 'ツ ', '◉_◉']
 
     const [emoji, setEmoji] = useState(
         emojies[Math.round(Math.random() * 12)]
@@ -14,7 +13,11 @@ export function Navbar() {
       <div id="nav-style">
           <img src={banner} alt="Website Banner" id="banner-logo" />
             <div id="nav-cont">
-                <h1 id="nav-title">TinyTweaks</h1>
+                <h1 id="nav-title">TinyTweaks <div id="nav-emoji" onClick={() => {
+                    setEmoji( emojies[Math.round(Math.random() * 12)] )
+                }}>
+                    <p>{emoji}</p>
+                </div></h1>
    
                 <nav id="nav-bar">
                     <p>&nbsp; <span id="nav-home">Home</span></p>
@@ -22,16 +25,6 @@ export function Navbar() {
                     <p>Apps</p>
                     <p>Settings &nbsp;</p>
                 </nav>
-           </div>
-
-           <div id="nav-emoji" onClick={() => {
-               setEmoji(
-                   emojies[Math.round(Math.random() * 12)]
-               )
-           }}>
-               <p id="nav-emote" title=":)">
-                   {emoji}
-               </p>
            </div>
 
            <div id="setting-btn">
